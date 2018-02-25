f6dc93ff0d55b46f47d8de023a93b5692efa569c
Wladimir Reinaga <tekkenucv@gmail.com>
2018-02-24 21:33:51 -0400

    agrege una variable para priobar commits

--- ExpressPrueba/routes/redir.js
+++ ExpressPrueba/routes/redir.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var controlMain = require('../controllers/main');
+var p = 6;
 
 /* GET nueva. */
 router.get('/', controlMain.do);
